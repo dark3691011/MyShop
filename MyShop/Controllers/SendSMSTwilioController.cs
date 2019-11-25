817d905bdad399add7f26a19bd3185be8ebb4e68
dark3691011 <37678562+dark3691011@users.noreply.github.com>
2019-11-25 14:43:17 +0700

    change library for rss

---
+++ MyShop/Controllers/SendSMSTwilioController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace MyShop.Controllers
+{
+    public class SendSMSTwilioController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Find your Account Sid and Token at twilio.com/console
+            // DANGER! This is insecure. See http://twil.io/secure
+            const string accountSid = "ACf5dbb7cda8e1717451df1d91ed110064";
+            const string authToken = "eda24973a6402c3dcea8d465741aaae5";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Hello Im Hai",
+                from: new Twilio.Types.PhoneNumber("+14844986307"),
+                to: new Twilio.Types.PhoneNumber("+84329791369")
+            );
+
+            return View();
+        }
+    }
+}
--- MyShop/ViewComponents/Rss.cs
+++ MyShop/ViewComponents/Rss.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CodeHollow.FeedReader;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,33 +15,9 @@ namespace MyShop.ViewComponents
         }
         public IViewComponentResult Invoke(string page)
         {
-            Chilkat.Rss rss = new Chilkat.Rss();
-
-            // Download from the feed URL:
-            bool success = rss.DownloadRss(page);
-            if (success != true)
-            {
-                return View();
-            }
-
-            // Get the 1st channel.
-            Chilkat.Rss rssChannel = rss.GetChannel(0);
-            if (rss.LastMethodSuccess == false)
-            {
-                return View();
-            }
+            Feed feed = FeedReader.Read(page);
             
-
-            // For each item in the channel, display the title, link,
-            // publish date, and categories assigned to the post.
-            int numItems = rssChannel.NumItems;
-            List<Chilkat.Rss> ListRss = new List<Chilkat.Rss>();
-            for (int i = 0; i <= numItems - 1; i++)
-            {
-                Chilkat.Rss rssItem = rssChannel.GetItem(i);
-                ListRss.Add(rssChannel.GetItem(i));
-            }
-            return View(ListRss);
+            return View(feed);
         }
     }
 }
